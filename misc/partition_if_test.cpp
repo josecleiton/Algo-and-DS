6d0b31e5cba2403132e4cb1a51726ce219feb163
josecleiton <jcleitonbc@gmail.com>
2018-12-29 10:09:54 -0300

    test mais completo

--- misc/partition_if_test.cpp
+++ misc/partition_if_test.cpp
@@ -4,7 +4,8 @@
 #include "partition_if.h"
 using namespace std;
 
-int main(){
+void randNumbers(void){
+    cout << endl << "RAND INTEGERS TEST" << endl << endl;
     int vet[50];
     int tam = sizeof(vet)/sizeof(*vet);
     // PREENCHENDO VETOR COM NUMEROS ALEATORIOS
@@ -15,9 +16,8 @@ int main(){
     //  PIVOT SERÁ 105
     vet[8] = vet[tam-1] = 105;
     cout << "PIVOT: " << vet[8] << endl;
-    int index = partition_if(vet, vet+tam-1, vet+8, \
-                    [](const int& a, const int& b){return a<=b;}) - vet;
-    cout << "Pivot's index: " << index << endl;
+    int index = partition_if(vet, vet+tam-1, vet+8, less_equal<int>()) - vet;
+    cout << "Pivot index (after partition_if): " << index << endl;
 
     // MOSTRANDO O INTERVALO
     for(int i=0; i<tam; i++)
@@ -26,9 +26,47 @@ int main(){
 
     // TESTANDO SE ESTÁ PARTICIONADO
     //cout << is_partitioned(vet, vet+tam-1, [](const int& a){return a <= 105;});
-    cout << "Is partitioned? " << is_partitioned_if(vet, vet+tam-1, vet+index, \
-                                    [](const int& a, const int& b){return a<=b;});
+    cout << "Is partitioned? " << is_partitioned_if(vet, vet+tam-1, vet+index, less_equal<int>());
     cout << endl;
+}
 
+void strings(void){
+    cout << endl << "STRING TEST" << endl << endl;
+    string vet[19] = {
+        "vertebrado",
+        "ave",
+        "zebra",
+        "mamifero",
+        "carnivoro",
+        "onivoro",
+        "herbivoro",
+        "invertebrado",
+        "inseto",
+        "anelideo",
+        "hematofago",
+        "pulga",
+        "lagarta",
+        "aguia",
+        "pomba",
+        "homem",
+        "vaca",
+        "sanguessuga",
+        "minhoca",
+    };
+    // escolhendo "zebra" como pivot (2)
+    int tam = sizeof(vet)/sizeof(*vet);
+    cout << "PIVOT: " << vet[2] << endl;
+    int index = partition_if(vet, vet+tam-1, vet+2, less_equal<string>()) - vet;
+    cout << "Pivot index (after partition_if): " << index << endl;
+    cout << endl << "Range: " << endl;
+    for(int i=0; i<tam; i++)
+        cout << vet[i] << endl; 
+    cout << endl << "Is partitioned? ";
+    cout << is_partitioned_if(vet, vet+tam-1, vet+2, less_equal<string>()) << endl;
+}
+
+int main(){
+    randNumbers();
+    strings();
     return 0;
 }
